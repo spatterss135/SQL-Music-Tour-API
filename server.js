5ce1e79ef9e1ecd3c6d5334c6d2a0fb24090b2c9
spatterss135 <spatterss135@gmail.com>
2022-02-09 17:38:39 -0600

    Connected to music-tour db

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize('music-tour', 'postgres', 'Strawhat987', {
+    dialect: 'postgres'
+})
+
+try {
+    sequelize.authenticate()
+    console.log('Connected to Postgres DB')
+
+} catch(err){
+    console.log(`Could not connect to DB due to error: ${err}`)
+}
 
 // ROOT
 app.get('/', (req, res) => {
@@ -14,7 +28,9 @@ app.get('/', (req, res) => {
     })
 })
 
+
+
 // LISTEN
 app.listen(process.env.PORT, () => {
-    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
+    console.log(`🎸 Rockin' on port: ${process.env.PG_URI}`)
 })
